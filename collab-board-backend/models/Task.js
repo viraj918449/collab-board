c1b913daf0e4547bcc5b90d62b95fd0eaeef4fce
Nadishan Vithanaarachchi <vnvithanaarachchi@students.nsbm.ac.lk>
2026-08-27 23:34:41 +0530

    Add task

--- collab-board-backend/models/Task.js
+++ collab-board-backend/models/Task.js
@@ -1,1 +1,32 @@
 // models/Task.js
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: { 
+    type: String, 
+    required: true 
+  },
+  tag: { 
+    type: String, 
+    required: true 
+  },
+  column: { 
+    type: String, 
+    default: 'todo', 
+    enum: ['todo', 'inprogress', 'done'] 
+  },
+  boardId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Board', 
+    required: true // Now every task MUST belong to a board
+  },
+  createdBy: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  }
+}, { 
+  timestamps: true 
+});
+
+module.exports = mongoose.model('Task', taskSchema);
